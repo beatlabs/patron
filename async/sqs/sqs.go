eff2336205f7e7ddee8e19d29e4c6a7cae89d7cd
Sotirios Mantziaris <smantziaris@gmail.com>
2019-07-30 23:26:45 +0300

    Fixed linting
    Signed-off-by: Sotirios Mantziaris <smantziaris@gmail.com>

--- async/sqs/sqs.go
+++ async/sqs/sqs.go
@@ -3,30 +3,42 @@ package sqs
 import (
 	"context"
 	"github.com/beatlabs/patron/async"
+	"github.com/beatlabs/patron/log"
 )
 
 type message struct {
 }
 
+// Context of the message.
 func (m *message) Context() context.Context {
 	panic("implement me")
 }
 
+// Decode the message to the provided argument.
 func (m *message) Decode(v interface{}) error {
 	panic("implement me")
 }
 
+// Ack the message.
 func (m *message) Ack() error {
-	panic("implement me")
+	// TODO: delete the message from SQS.
+	// TODO: metrics
+	return nil
 }
 
+// Nack the message. SQS does not support Nack, the message will be available after the visibility timeout has passed.
 func (m *message) Nack() error {
-	panic("implement me")
+	// TODO: use the correct message id from SQS
+	// TODO: metrics
+	log.Debugf("message %s not deleted, will be available", "1")
+	return nil
 }
 
+// Factory for creating SQS consumers.
 type Factory struct {
 }
 
+// Create a new SQS consumer.
 func (f *Factory) Create() (async.Consumer, error) {
 	panic("implement me")
 }
@@ -34,10 +46,12 @@ func (f *Factory) Create() (async.Consumer, error) {
 type consumer struct {
 }
 
+// Consume messages from SQS and send them to the channel.
 func (c *consumer) Consume(context.Context) (<-chan async.Message, <-chan error, error) {
 	panic("implement me")
 }
 
+// Close the consumer.
 func (c *consumer) Close() error {
 	panic("implement me")
 }

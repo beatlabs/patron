12de1a436e0be921f33eb379a56652e0813c2d70
Sotirios Mantziaris <smantziaris@gmail.com>
2019-08-06 18:38:03 +0300

    Added tests and localstack

--- async/sqs/sqs_test.go
+++ async/sqs/sqs_test.go
@@ -3,6 +3,8 @@ package sqs
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -45,6 +47,56 @@ func TestNewConfig(t *testing.T) {
 				assert.Equal(t, tt.args.id, got.id)
 				assert.Equal(t, tt.args.secret, got.secret)
 				assert.Equal(t, tt.args.token, got.token)
+			}
+		})
+	}
+}
+
+func TestNewFactory(t *testing.T) {
+	cfg, err := NewConfig("region", "id", "secret", "token")
+	require.NoError(t, err)
+	type args struct {
+		cfg   Config
+		queue string
+		oo    []OptionFunc
+	}
+	tests := map[string]struct {
+		args        args
+		expectedErr string
+	}{
+		"success": {
+			args: args{
+				cfg:   *cfg,
+				queue: "queue",
+				oo:    []OptionFunc{MaxMessages(1)},
+			},
+		},
+		"missing queue": {
+			args: args{
+				cfg:   *cfg,
+				queue: "",
+				oo:    []OptionFunc{MaxMessages(1)},
+			},
+			expectedErr: "queue name is empty",
+		},
+		"invalid option": {
+			args: args{
+				cfg:   *cfg,
+				queue: "queue",
+				oo:    []OptionFunc{MaxMessages(-1)},
+			},
+			expectedErr: "max messages should be between 1 and 10",
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, err := NewFactory(tt.args.cfg, tt.args.queue, tt.args.oo...)
+			if tt.expectedErr != "" {
+				assert.EqualError(t, err, tt.expectedErr)
+				assert.Nil(t, got)
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, got)
 			}
 		})
 	}

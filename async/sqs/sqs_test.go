176339518926e43150a262ce1a9a065a7e61e233
Sotirios Mantziaris <smantziaris@gmail.com>
2019-08-07 23:17:19 +0300

    Fixed broken tests
    Signed-off-by: Sotirios Mantziaris <smantziaris@gmail.com>

--- async/sqs/sqs_test.go
+++ async/sqs/sqs_test.go
@@ -13,31 +13,30 @@ func TestNewConfig(t *testing.T) {
 		region string
 		id     string
 		secret string
-		token  string
 	}
 	tests := map[string]struct {
 		args        args
 		expectedErr string
 	}{
 		"success": {
-			args: args{region: "region", id: "id", secret: "secret", token: "token"},
+			args: args{region: "region", id: "id", secret: "secret"},
 		},
 		"missing region": {
-			args:        args{region: "", id: "id", secret: "secret", token: "token"},
+			args:        args{region: "", id: "id", secret: "secret"},
 			expectedErr: "AWS region not provided",
 		},
 		"missing id": {
-			args:        args{region: "region", id: "", secret: "secret", token: "token"},
+			args:        args{region: "region", id: "", secret: "secret"},
 			expectedErr: "AWS id not provided",
 		},
 		"missing secret": {
-			args:        args{region: "region", id: "id", secret: "", token: "token"},
+			args:        args{region: "region", id: "id", secret: ""},
 			expectedErr: "AWS secret not provided",
 		},
 	}
 	for name, tt := range tests {
 		t.Run(name, func(t *testing.T) {
-			got, err := NewConfig(tt.args.region, tt.args.id, tt.args.secret, tt.args.token)
+			got, err := NewConfig(tt.args.region, tt.args.id, tt.args.secret, "token", "endpoint")
 			if tt.expectedErr != "" {
 				assert.EqualError(t, err, tt.expectedErr)
 				assert.Nil(t, got)
@@ -46,14 +45,15 @@ func TestNewConfig(t *testing.T) {
 				assert.Equal(t, tt.args.region, got.region)
 				assert.Equal(t, tt.args.id, got.id)
 				assert.Equal(t, tt.args.secret, got.secret)
-				assert.Equal(t, tt.args.token, got.token)
+				assert.Equal(t, "token", got.token)
+				assert.Equal(t, "endpoint", got.endpoint)
 			}
 		})
 	}
 }
 
 func TestNewFactory(t *testing.T) {
-	cfg, err := NewConfig("region", "id", "secret", "token")
+	cfg, err := NewConfig("region", "id", "secret", "token", "")
 	require.NoError(t, err)
 	type args struct {
 		cfg   Config

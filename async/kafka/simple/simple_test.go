b24ad17511be9f03cc4ab684b4312d3355e54e06
Kostas Stamatakis <moukoublen@gmail.com>
2019-12-31 13:42:18 +0200

    add tests for simple factory

--- async/kafka/simple/simple_test.go
+++ async/kafka/simple/simple_test.go
@@ -5,8 +5,89 @@ import (
 	"testing"
 
 	"github.com/Shopify/sarama"
+	"github.com/beatlabs/patron/async/kafka"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestNew(t *testing.T) {
+	brokers := []string{"192.168.1.1"}
+	type args struct {
+		name    string
+		brokers []string
+		topic   string
+		options []kafka.OptionFunc
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name:    "fails with missing name",
+			args:    args{name: "", brokers: brokers, topic: "topic1"},
+			wantErr: true,
+		},
+		{
+			name:    "fails with missing brokers",
+			args:    args{name: "test", brokers: []string{}, topic: "topic1"},
+			wantErr: true,
+		},
+		{
+			name:    "fails with missing topics",
+			args:    args{name: "test", brokers: brokers, topic: ""},
+			wantErr: true,
+		},
+		{
+			name:    "success",
+			args:    args{name: "test", brokers: brokers, topic: "topic1"},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := New(tt.args.name, tt.args.topic, tt.args.brokers, tt.args.options...)
+			if tt.wantErr {
+				assert.Error(t, err)
+				assert.Nil(t, got)
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, got)
+			}
+		})
+	}
+}
+
+func TestFactory_Create(t *testing.T) {
+	type fields struct {
+		oo []kafka.OptionFunc
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{name: "success", wantErr: false},
+		{name: "failed with invalid option", fields: fields{oo: []kafka.OptionFunc{kafka.Buffer(-100)}}, wantErr: true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &Factory{
+				name:    "test",
+				topic:   "topic",
+				brokers: []string{"192.168.1.1"},
+				oo:      tt.fields.oo,
+			}
+			got, err := f.Create()
+			if tt.wantErr {
+				assert.Error(t, err)
+				assert.Nil(t, got)
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, got)
+			}
+		})
+	}
+}
 
 func TestConsumer_ConsumeWithoutGroup(t *testing.T) {
 	broker := sarama.NewMockBroker(t, 0)

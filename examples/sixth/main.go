ef0ed283c0b2bc308364c71a9401054b04898f87
gm42 <gm42@users.noreply.github.com>
2020-03-05 13:58:17 +0100

    Move packages to old path

--- examples/sixth/main.go
+++ examples/sixth/main.go
@@ -7,14 +7,13 @@ import (
 	"os"
 	"time"
 
-	"github.com/beatlabs/patron/trace/sync/kafka"
-
 	"github.com/beatlabs/patron"
 	"github.com/beatlabs/patron/examples"
 	"github.com/beatlabs/patron/log"
 	"github.com/beatlabs/patron/sync"
 	patronhttp "github.com/beatlabs/patron/sync/http"
 	tracehttp "github.com/beatlabs/patron/trace/http"
+	"github.com/beatlabs/patron/trace/sync/kafka"
 )
 
 const (

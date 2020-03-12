529649134b081d4629a5c66b9c8531ed4c23cc30
GLampadakis <glampadakis@thebeat.co>
2020-03-12 12:44:52 +0200

    log the error that is closing a consumer group
    Signed-off-by: Giorgos Lampadakis <glampadakis@thebeat.co>

--- async/kafka/group/group.go
+++ async/kafka/group/group.go
@@ -122,6 +122,7 @@ func (c *consumer) Consume(ctx context.Context) (<-chan async.Message, <-chan er
 				closeConsumer(c.cg)
 				return
 			case consumerError := <-c.cg.Errors():
+				log.Warn("closing consumer group ", <-c.cg.Errors())
 				closeConsumer(c.cg)
 				chErr <- consumerError
 				return

8674de649e3de851f6cfdc770937054cedf30740
Vangelis Katikaridis <v.katikaridis@thebeat.co>
2020-04-15 14:15:13 +0200

    #128 implement review comments
    Signed-off-by: Vangelis Katikaridis <v.katikaridis@thebeat.co>

--- component/http/route_cache.go
+++ component/http/route_cache.go
@@ -8,6 +8,7 @@ import (
 	"github.com/beatlabs/patron/log"
 )
 
+// wrapProcessorFunc wraps the processing func with the cache handler interface
 func wrapProcessorFunc(path string, processor ProcessorFunc, rc *routeCache) ProcessorFunc {
 	return func(ctx context.Context, request *Request) (response *Response, e error) {
 		// we are doing the opposite work that we would do in the processor,
@@ -22,7 +23,7 @@ func wrapProcessorFunc(path string, processor ProcessorFunc, rc *routeCache) Pro
 }
 
 // processorExecutor is the function that will create a new cachedResponse based on a ProcessorFunc implementation
-var processorExecutor = func(ctx context.Context, request *Request, hnd ProcessorFunc) executor {
+func processorExecutor(ctx context.Context, request *Request, hnd ProcessorFunc) executor {
 	return func(now int64, key string) *cachedResponse {
 		var err error
 		response, err := hnd(ctx, request)
@@ -40,6 +41,7 @@ var processorExecutor = func(ctx context.Context, request *Request, hnd Processo
 	}
 }
 
+// wrapHandlerFunc wraps the handler func with the cache handler interface
 func wrapHandlerFunc(handler http.HandlerFunc, rc *routeCache) http.HandlerFunc {
 	return func(response http.ResponseWriter, request *http.Request) {
 		req := fromHTTPRequest(request)
@@ -55,7 +57,7 @@ func wrapHandlerFunc(handler http.HandlerFunc, rc *routeCache) http.HandlerFunc 
 }
 
 // handlerExecutor is the function that will create a new cachedResponse based on a HandlerFunc implementation
-var handlerExecutor = func(response http.ResponseWriter, request *http.Request, hnd http.HandlerFunc) executor {
+func handlerExecutor(_ http.ResponseWriter, request *http.Request, hnd http.HandlerFunc) executor {
 	return func(now int64, key string) *cachedResponse {
 		var err error
 		responseReadWriter := NewResponseReadWriter()

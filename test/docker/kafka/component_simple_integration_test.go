5e8b6740bf729c9ac2cba26ba29b4931673a15ec
teivah <t.harsanyi@thebeat.co>
2021-10-09 15:43:32 +0200

    DurationOffset component test
    Signed-off-by: teivah <t.harsanyi@thebeat.co>

--- test/docker/kafka/component_simple_integration_test.go
+++ test/docker/kafka/component_simple_integration_test.go
@@ -5,11 +5,9 @@ package kafka
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"strconv"
 	"sync"
-	"sync/atomic"
 	"testing"
 	"time"
 
@@ -82,13 +80,12 @@ func TestKafkaComponentSimple_Success(t *testing.T) {
 	patronWG.Wait()
 }
 
-func TestKafkaComponentSimple_FailOnceAndRetry(t *testing.T) {
+func TestKafkaComponentSimple_DurationOffset(t *testing.T) {
 	// Test parameters
-	numOfMessagesToSend := 100
+	numOfMessagesToSend := 5
 
-	// Set up the component
-	didFail := int32(0)
-	actualMessages := make([]string, 0)
+	// Set up the kafka component
+	actualSuccessfulMessages := make([]string, 0)
 	var consumerWG sync.WaitGroup
 	consumerWG.Add(numOfMessagesToSend)
 	processorFunc := func(batch kafka.Batch) error {
@@ -96,75 +93,79 @@ func TestKafkaComponentSimple_FailOnceAndRetry(t *testing.T) {
 			var msgContent string
 			err := decodeString(msg.Message().Value, &msgContent)
 			assert.NoError(t, err)
-
-			if msgContent == "50" && atomic.CompareAndSwapInt32(&didFail, 0, 1) {
-				return errors.New("expected error")
-			}
+			actualSuccessfulMessages = append(actualSuccessfulMessages, msgContent)
 			consumerWG.Done()
-			actualMessages = append(actualMessages, msgContent)
 		}
 		return nil
 	}
-	component := newSimpleComponent(t, failAndRetryTopic3, 3, 1, processorFunc)
+	component := newSimpleComponent(t, successTopic3, 3, 10, processorFunc, simple.DurationOffset(4*time.Hour, timestampExtractor))
 
-	// Send messages to the kafka topic
-	var producerWG sync.WaitGroup
-	producerWG.Add(1)
-	go func() {
-		producer, err := NewProducer()
-		require.NoError(t, err)
-		for i := 1; i <= numOfMessagesToSend; i++ {
-			_, _, err := producer.SendMessage(&sarama.ProducerMessage{Topic: failAndRetryTopic3, Value: sarama.StringEncoder(strconv.Itoa(i))})
-			require.NoError(t, err)
-		}
-		producerWG.Done()
-	}()
-
-	// Run Patron with the component
+	// Run Patron with the kafka component
 	patronContext, patronCancel := context.WithCancel(context.Background())
 	var patronWG sync.WaitGroup
 	patronWG.Add(1)
 	go func() {
-		svc, err := patron.New(failAndRetryTopic3, "0", patron.LogFields(map[string]interface{}{"test": failAndRetryTopic3}))
+		svc, err := patron.New(successTopic3, "0", patron.LogFields(map[string]interface{}{"test": successTopic3}))
 		require.NoError(t, err)
 		err = svc.WithComponents(component).Run(patronContext)
 		require.NoError(t, err)
 		patronWG.Done()
 	}()
 
-	// Wait for the producer & consumer to finish
+	// Send messages to the kafka topic
+	var producerWG sync.WaitGroup
+	producerWG.Add(1)
+	now := time.Now()
+	messages := createTimestampPayload(
+		now.Add(-10*time.Hour),
+		now.Add(-5*time.Hour),
+		now.Add(-3*time.Hour),
+		now.Add(-2*time.Hour),
+		now.Add(-1*time.Hour),
+	)
+	go func() {
+		producer, err := NewProducer()
+		require.NoError(t, err)
+		for _, val := range messages {
+			_, _, err := producer.SendMessage(getProducerMessage(simpleTopic3, val))
+			require.NoError(t, err)
+		}
+		producerWG.Done()
+	}()
+
+	// Wait for both consumer and producer to finish processing all the messages.
 	producerWG.Wait()
 	consumerWG.Wait()
+
+	// Verify all messages were processed in the right order
+	assert.Equal(t, messages[2:], actualSuccessfulMessages)
 
 	// Shutdown Patron and wait for it to finish
 	patronCancel()
 	patronWG.Wait()
-
-	// Verify all messages were processed in the right order
-	expectedMessages := make([]string, numOfMessagesToSend)
-	for i := 0; i < numOfMessagesToSend; i++ {
-		expectedMessages[i] = strconv.Itoa(i + 1)
-	}
-	assert.Equal(t, expectedMessages, actualMessages)
 }
 
-func newSimpleComponent(t *testing.T, name string, retries uint, batchSize uint, processorFunc kafka.BatchProcessorFunc) *simple.Component {
+func newSimpleComponent(t *testing.T, name string, retries uint, batchSize uint, processorFunc kafka.BatchProcessorFunc, oo ...simple.OptionFunc) *simple.Component {
 	saramaCfg := sarama.NewConfig()
 	saramaCfg.Consumer.Offsets.Initial = sarama.OffsetOldest
 	saramaCfg.Version = sarama.V2_6_0_0
 
 	broker := fmt.Sprintf("%s:%s", kafkaHost, kafkaPort)
+	options := []simple.OptionFunc{
+		simple.FailureStrategy(kafka.ExitStrategy),
+		simple.BatchSize(batchSize),
+		simple.BatchTimeout(100 * time.Millisecond),
+		simple.Retries(retries),
+		simple.RetryWait(200 * time.Millisecond),
+		simple.SaramaConfig(saramaCfg),
+	}
+	options = append(options, oo...)
 	cmp, err := simple.New(
 		name,
 		[]string{broker},
 		name,
 		processorFunc,
-		simple.FailureStrategy(kafka.ExitStrategy),
-		simple.BatchSize(batchSize),
-		simple.BatchTimeout(100*time.Millisecond),
-		simple.Retries(retries),
-		simple.RetryWait(200*time.Millisecond),
-		simple.SaramaConfig(saramaCfg),
+		options...,
 	)
 	require.NoError(t, err)
 

3a75c6fb3ae88a74b9bb68b9b7f7766700d66b2c
Sotirios Mantziaris <smantziaris@gmail.com>
2021-12-25 20:58:06 +0200

    HTTP v2

--- component/http/v2/router/httprouter/router_test.go
+++ component/http/v2/router/httprouter/router_test.go
@@ -1,10 +1,42 @@
 package httprouter
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestNew(t *testing.T) {
+	t.Parallel()
+	route, err := NewRawRoute(http.MethodGet, "/api/", func(writer http.ResponseWriter, request *http.Request) {
+		writer.WriteHeader(200)
+	})
+	require.NoError(t, err)
+	type args struct {
+		oo []OptionFunc
+	}
+	tests := map[string]struct {
+		args        args
+		expectedErr string
+	}{
+		"success":            {args: args{oo: []OptionFunc{Routes(route)}}},
+		"option func failed": {args: args{oo: []OptionFunc{AliveCheck(nil)}}, expectedErr: "alive check function is nil"},
+	}
+	for name, tt := range tests {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			got, err := New(tt.args.oo...)
+			if tt.expectedErr != "" {
+				assert.EqualError(t, err, tt.expectedErr)
+			} else {
+				assert.NotNil(t, got)
+			}
+		})
+	}
+}
 
 func TestRoutes(t *testing.T) {
 	t.Parallel()

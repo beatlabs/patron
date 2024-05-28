84ef31f09a7e5fd7b5a19c72a3d9a81ace02512f
Sotirios Mantziaris <smantziaris@gmail.com>
2024-05-28 23:47:27 +0300

    chore: Update elasticsearch client with tracing capabilities

--- client/es/elasticsearch.go
+++ client/es/elasticsearch.go
@@ -2,39 +2,12 @@
 package es
 
 import (
-	"bytes"
-	"fmt"
-	"io"
-	"net/http"
-	"net/url"
-	"os"
-	"strconv"
-	"strings"
-	"time"
-
-	"github.com/beatlabs/patron/observability/log"
-	"github.com/beatlabs/patron/trace"
 	"github.com/elastic/elastic-transport-go/v8/elastictransport"
 	"github.com/elastic/go-elasticsearch/v8"
-	"github.com/elastic/go-elasticsearch/v8/esapi"
-	"github.com/opentracing/opentracing-go"
-	"github.com/opentracing/opentracing-go/ext"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-const (
-	defaultHostEnv = "PATRON_ES_DEFAULT_HOST"
-	defaultPortEnv = "PATRON_ES_DEFAULT_PORT"
-
-	defaultHost = "http://localhost"
-	defaultPort = "9200"
-
-	respondentTag = "respondent"
-
-	opName  = "Elasticsearch Call"
-	cmpName = "go-elasticsearch"
-)
-
+// TODO: introduce metrics
 var reqDurationMetrics *prometheus.HistogramVec
 
 func init() {
@@ -50,168 +23,10 @@ func init() {
 	prometheus.MustRegister(reqDurationMetrics)
 }
 
-type tracingInfo struct {
-	user  string
-	hosts []string
-}
+// New creates a new elasticsearch client with tracing capabilities.
+func New(cfg elasticsearch.Config, version string) (*elasticsearch.Client, error) {
+	// TODO: metrics integration
+	cfg.Instrumentation = elastictransport.NewOtelInstrumentation(nil, false, version)
 
-func (t *tracingInfo) startSpan(req *http.Request) (opentracing.Span, error) {
-	if req == nil {
-		return nil, fmt.Errorf("request is empty")
-	}
-
-	uri := req.URL.RequestURI()
-	method := req.Method
-
-	var bodyFmt string
-	if req.Body != nil {
-		if rawBody, err := io.ReadAll(req.Body); err == nil {
-			req.Body = io.NopCloser(bytes.NewReader(rawBody))
-			bodyFmt = string(rawBody)
-		}
-	}
-	// TODO: need to change this to OT span
-	sp, _ := opentracing.StartSpanFromContext(req.Context(), opName)
-	ext.Component.Set(sp, cmpName)
-	ext.DBType.Set(sp, "elasticsearch")
-	ext.DBUser.Set(sp, t.user)
-
-	ext.HTTPUrl.Set(sp, uri)
-	ext.HTTPMethod.Set(sp, method)
-	ext.DBStatement.Set(sp, bodyFmt)
-
-	hostsFmt := "[" + strings.Join(t.hosts, ", ") + "]"
-	sp.SetTag(trace.HostsTag, hostsFmt)
-	sp.SetTag(trace.VersionTag, trace.Version)
-
-	return sp, nil
-}
-
-func endSpan(sp opentracing.Span, rsp *http.Response) {
-	// In cases where more than one host is given, the selected one is only known at this time
-	sp.SetTag(respondentTag, rsp.Request.URL.Host)
-
-	ext.HTTPStatusCode.Set(sp, uint16(rsp.StatusCode))
-	ext.Error.Set(sp, rsp.StatusCode >= http.StatusInternalServerError)
-	sp.Finish()
-}
-
-type transportClient struct {
-	client *elastictransport.Client
-	tracingInfo
-}
-
-// Perform wraps elasticsearch Perform with tracing functionality.
-func (c *transportClient) Perform(req *http.Request) (*http.Response, error) {
-	sp, err := c.startSpan(req)
-	if err != nil {
-		log.FromContext(req.Context()).Error("failed to start span", log.ErrorAttr(err))
-	}
-	start := time.Now()
-	rsp, err := c.client.Perform(req)
-	if err != nil || rsp == nil {
-		trace.SpanError(sp)
-		return rsp, err
-	}
-
-	observeResponse(req, sp, rsp, start)
-	return rsp, nil
-}
-
-func observeResponse(req *http.Request, sp opentracing.Span, rsp *http.Response, start time.Time) {
-	endSpan(sp, rsp)
-	durationHistogram := trace.Histogram{
-		Observer: reqDurationMetrics.WithLabelValues(req.Method, req.URL.Host, strconv.Itoa(rsp.StatusCode)),
-	}
-	durationHistogram.Observe(req.Context(), time.Since(start).Seconds())
-}
-
-// Config is a wrapper for elasticsearch.Config.
-type Config elasticsearch.Config
-
-// Client is a wrapper for elasticsearch.Client.
-type Client struct {
-	elasticsearch.Client
-}
-
-// NewDefaultClient returns an empty ES client with sane defaults.
-func NewDefaultClient() (*Client, error) {
-	return NewClient(Config{})
-}
-
-// NewClient is a modified version of elasticsearch.NewClient
-// that injects a tracing-ready transport.
-func NewClient(cfg Config) (*Client, error) {
-	urls, err := addrsToURLs(cfg.Addresses)
-	if err != nil {
-		return nil, fmt.Errorf("cannot create client: %w", err)
-	}
-
-	if len(urls) == 0 {
-		// Fallback to default values
-		addr := getAddrFromEnv()
-		u, err := url.Parse(addr)
-		if err != nil {
-			return nil, err
-		}
-		urls = append(urls, u)
-		cfg.Addresses = append(cfg.Addresses, addr)
-	}
-
-	esTransportClient, err := elastictransport.New(elastictransport.Config{
-		URLs:     urls,
-		Username: cfg.Username,
-		Password: cfg.Password,
-		APIKey:   cfg.APIKey,
-
-		Transport: cfg.Transport,
-		Logger:    cfg.Logger,
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	tracingInfo := tracingInfo{
-		user:  cfg.Username,
-		hosts: cfg.Addresses,
-	}
-	tp := &transportClient{
-		client:      esTransportClient,
-		tracingInfo: tracingInfo,
-	}
-
-	return &Client{
-		elasticsearch.Client{
-			BaseClient: elasticsearch.BaseClient{
-				Transport: tp,
-			},
-			API: esapi.New(tp),
-		},
-	}, nil
-}
-
-func addrsToURLs(addrs []string) ([]*url.URL, error) {
-	urls := make([]*url.URL, 0, len(addrs))
-	for _, addr := range addrs {
-		u, err := url.Parse(strings.TrimRight(addr, "/"))
-		if err != nil {
-			return nil, fmt.Errorf("cannot parse url: %w", err)
-		}
-
-		urls = append(urls, u)
-	}
-	return urls, nil
-}
-
-func getAddrFromEnv() string {
-	host, found := os.LookupEnv(defaultHostEnv)
-	if !found {
-		host = defaultHost
-	}
-	port, found := os.LookupEnv(defaultPortEnv)
-	if !found {
-		port = defaultPort
-	}
-
-	return host + ":" + port
+	return elasticsearch.NewClient(cfg)
 }
--- client/es/elasticsearch_test.go
+++ client/es/elasticsearch_test.go
@@ -5,150 +5,29 @@ import (
 	"net"
 	"net/http"
 	"net/http/httptest"
-	"net/url"
-	"os"
 	"strings"
 	"testing"
 
-	"github.com/beatlabs/patron/trace"
-	"github.com/elastic/elastic-transport-go/v8/elastictransport"
+	patrontrace "github.com/beatlabs/patron/observability/trace"
 	"github.com/elastic/go-elasticsearch/v8"
-	"github.com/opentracing/opentracing-go"
-	"github.com/opentracing/opentracing-go/mocktracer"
-	"github.com/prometheus/client_golang/prometheus/testutil"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/otel/sdk/trace/tracetest"
 )
 
-func TestStartEndSpan(t *testing.T) {
-	defaultAddr := getAddrFromEnv()
-	hosts := []string{defaultAddr, "http://10.1.1.1:9200"}
-	body, user, method := `{"field1": "10"}`, "user1", "PUT"
-
-	req, err := http.NewRequest(method, defaultAddr, strings.NewReader(body))
-	assert.NoError(t, err)
-
-	tracingInfo := tracingInfo{
-		user:  user,
-		hosts: hosts,
-	}
-	mtr := mocktracer.New()
-	opentracing.SetGlobalTracer(mtr)
-
-	sp, err := tracingInfo.startSpan(req)
-	assert.NoError(t, err)
-	assert.NotNil(t, sp)
-	assert.Empty(t, mtr.FinishedSpans())
-	assert.IsType(t, &mocktracer.MockSpan{}, sp)
-
-	jsp, ok := sp.(*mocktracer.MockSpan)
-	assert.True(t, ok)
-	assert.NotNil(t, jsp)
-	actualTags := jsp.Tags()
-
-	assert.Equal(t, body, actualTags["db.statement"])
-
-	hostsFmt := "[" + strings.Join(hosts, ", ") + "]"
-	assert.EqualValues(t, hostsFmt, actualTags["hosts"])
-
-	assert.Equal(t, "dev", actualTags["version"])
-	assert.Equal(t, "go-elasticsearch", actualTags["component"])
-	assert.Equal(t, "elasticsearch", actualTags["db.type"])
-	assert.Equal(t, user, actualTags["db.user"])
-	assert.Equal(t, "/", actualTags["http.url"])
-	assert.Equal(t, method, actualTags["http.method"])
-
-	respondent := "es.respondent.com:9200"
-	statusCode := 200
-	rsp := &http.Response{
-		Request: &http.Request{
-			URL: &url.URL{
-				Host: respondent,
-			},
-		},
-		StatusCode: statusCode,
-	}
-	endSpan(sp, rsp)
-
-	jsp, ok = sp.(*mocktracer.MockSpan)
-	assert.True(t, ok)
-	assert.Equal(t, respondent, jsp.Tag(respondentTag))
-	assert.Equal(t, uint16(statusCode), jsp.Tag("http.status_code"))
-	assert.Equal(t, false, jsp.Tag("error"))
-
-	actualResponseTags := jsp.Tags()
-	delete(actualResponseTags, "http.status_code")
-	delete(actualResponseTags, respondentTag)
-	delete(actualResponseTags, "error")
-	assert.EqualValues(t, actualTags, actualResponseTags)
-}
-
-func TestNewDefaultClient(t *testing.T) {
-	newClient, err := NewDefaultClient()
-	assert.NoError(t, err)
-
-	upstreamClient, err := elasticsearch.NewDefaultClient()
-	assert.NoError(t, err)
-	assert.IsType(t, *upstreamClient, newClient.Client) // nolint:govet
-
-	expectedTransport, transport := new(transportClient), newClient.Transport
-	assert.IsType(t, expectedTransport, transport)
-
-	defaultAddr := getAddrFromEnv()
-	expectedURL, err := url.Parse(strings.TrimRight(defaultAddr, "/"))
-	assert.NoError(t, err)
-	cfg := elastictransport.Config{
-		URLs:      []*url.URL{expectedURL},
-		Transport: nil,
-	}
-	expectedTransport.client, err = elastictransport.New(cfg)
-	assert.NoError(t, err)
-	assert.NotNil(t, expectedTransport.client)
-}
-
-func TestNewClient(t *testing.T) {
-	addresses := []string{"http://www.host1.com:9200", "https://10.1.1.1:9300"}
-	user, password, apiKey := "user1", "pass", "key"
-	cfg := Config{
-		Addresses: addresses,
-		Username:  user,
-		Password:  password,
-		APIKey:    apiKey,
-	}
-
-	newClient, err := NewClient(cfg)
-	assert.NoError(t, err)
-	assert.IsType(t, new(Client), newClient)
-
-	expectedTransport, transport := new(transportClient), newClient.Transport
-	assert.IsType(t, expectedTransport, transport)
-
-	expectedURLs, err := addrsToURLs(addresses)
-	assert.NoError(t, err)
-	transportCfg := elastictransport.Config{
-		URLs:      expectedURLs,
-		Username:  user,
-		Password:  password,
-		APIKey:    apiKey,
-		Transport: nil,
-		Logger:    nil,
-	}
-	expectedTransport.client, err = elastictransport.New(transportCfg)
-	assert.NoError(t, err)
-	assert.NotNil(t, expectedTransport.client)
-}
-
-func TestEsQuery(t *testing.T) {
-	mtr := mocktracer.New()
-	opentracing.SetGlobalTracer(mtr)
-	assert.Empty(t, mtr.FinishedSpans())
+func TestNew(t *testing.T) {
+	exp := tracetest.NewInMemoryExporter()
+	tracePublisher, err := patrontrace.Setup("test", nil, exp)
+	require.NoError(t, err)
 
 	responseMsg := `[{"acknowledged": true, "shards_acknowledged": true, "index": "test"}]`
 	ctx, indexName := context.Background(), "test_index"
 	ts := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.Header().Add("X-Elastic-Product", "Elasticsearch")
 		_, err := w.Write([]byte(responseMsg))
 		assert.NoError(t, err)
 	}))
-	listener, err := net.Listen("tcp", ":"+defaultPort)
+	listener, err := net.Listen("tcp", ":9200")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -156,81 +35,28 @@ func TestEsQuery(t *testing.T) {
 	ts.Start()
 	defer ts.Close()
 
-	queryBody := `{"mappings": {"_doc": {"properties": {"field1": {"type": "integer"}}}}}`
-	esClient, err := NewDefaultClient()
+	host := "http://localhost:9200"
+
+	cfg := elasticsearch.Config{
+		Addresses: []string{host},
+	}
+
+	version := "1.0.0"
+	client, err := New(cfg, version)
 	assert.NoError(t, err)
-	rsp, err := esClient.Indices.Create(
+	assert.NotNil(t, client)
+
+	queryBody := `{"mappings": {"_doc": {"properties": {"field1": {"type": "integer"}}}}}`
+
+	rsp, err := client.Indices.Create(
 		indexName,
-		esClient.Indices.Create.WithBody(strings.NewReader(queryBody)),
-		esClient.Indices.Create.WithContext(ctx),
+		client.Indices.Create.WithBody(strings.NewReader(queryBody)),
+		client.Indices.Create.WithContext(ctx),
 	)
 	assert.NoError(t, err)
 	assert.NotNil(t, rsp)
 
-	// assert span
-	finishedSpans := mtr.FinishedSpans()
-	assert.Equal(t, 1, len(finishedSpans))
-	expected := map[string]interface{}{
-		"component":        "go-elasticsearch",
-		"db.statement":     "{\"mappings\": {\"_doc\": {\"properties\": {\"field1\": {\"type\": \"integer\"}}}}}",
-		"db.type":          "elasticsearch",
-		"db.user":          "",
-		"error":            false,
-		"hosts":            "[http://localhost:9200]",
-		"http.method":      "PUT",
-		"http.status_code": uint16(200),
-		"http.url":         "/test_index",
-		"respondent":       "localhost:9200",
-		"version":          "dev",
-	}
-	assert.Equal(t, expected, finishedSpans[0].Tags())
-	assert.Equal(t, opName, finishedSpans[0].OperationName)
+	assert.NoError(t, tracePublisher.ForceFlush(context.Background()))
 
-	// assert metrics
-	assert.Equal(t, 1, testutil.CollectAndCount(reqDurationMetrics, "client_elasticsearch_request_duration_seconds"))
-}
-
-func TestGetAddrFromEnv(t *testing.T) {
-	addr := getAddrFromEnv()
-	assert.Equal(t, defaultHost+":"+defaultPort, addr)
-
-	assert.NoError(t, os.Setenv(defaultHostEnv, "http://10.1.1.1"))
-	assert.NoError(t, os.Setenv(defaultPortEnv, "9300"))
-
-	addr = getAddrFromEnv()
-	assert.Equal(t, "http://10.1.1.1:9300", addr)
-}
-
-func TestStartSpan(t *testing.T) {
-	mtr := mocktracer.New()
-	opentracing.SetGlobalTracer(mtr)
-
-	hostPool := []string{"http://localhost:9200", "http:10.1.1.1:9201", "https://www.domain.com:9203"}
-	tracingInfo := tracingInfo{
-		user:  "es-user",
-		hosts: hostPool,
-	}
-	req, err := http.NewRequest("query-method", "es-uri", strings.NewReader("query-body"))
-	assert.NoError(t, err)
-
-	sp, err := tracingInfo.startSpan(req)
-	assert.NoError(t, err)
-	assert.NotNil(t, sp)
-	assert.IsType(t, &mocktracer.MockSpan{}, sp)
-	jsp, ok := sp.(*mocktracer.MockSpan)
-	assert.True(t, ok)
-	assert.NotNil(t, jsp)
-	trace.SpanSuccess(sp)
-	rawspan := mtr.FinishedSpans()[0]
-	assert.Equal(t, map[string]interface{}{
-		"component":    "go-elasticsearch",
-		"version":      "dev",
-		"db.statement": "query-body",
-		"db.type":      "elasticsearch",
-		"db.user":      "es-user",
-		"http.url":     "es-uri",
-		"http.method":  "query-method",
-		trace.HostsTag: "[http://localhost:9200, http:10.1.1.1:9201, https://www.domain.com:9203]",
-		"error":        false,
-	}, rawspan.Tags())
+	assert.Len(t, exp.GetSpans(), 1)
 }

919e86d22411934a2cad5e8b770ec866cce36758
Sotirios Mantziaris <smantziaris@gmail.com>
2024-05-30 20:04:00 +0300

    chore: Introduce tracing capabilities in integration_test.go

--- client/mongo/integration_test.go
+++ client/mongo/integration_test.go
@@ -7,26 +7,39 @@ import (
 	"context"
 	"testing"
 
+	patrontrace "github.com/beatlabs/patron/observability/trace"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.opentelemetry.io/otel/sdk/trace/tracetest"
 )
 
-// TODO: assert metrics and traces
+// TODO: introduce metrics?
 
 func TestConnectAndExecute(t *testing.T) {
-	client, err := Connect(context.Background())
+	exp := tracetest.NewInMemoryExporter()
+	tracePublisher, err := patrontrace.Setup("test", nil, exp)
+	require.NoError(t, err)
+	ctx := context.Background()
+
+	client, err := Connect(ctx)
 	assert.NoError(t, err)
 	assert.NotNil(t, client)
 
 	t.Run("success", func(t *testing.T) {
-		err = client.Ping(context.Background(), nil)
+		exp.Reset()
+		err = client.Ping(ctx, nil)
 		require.NoError(t, err)
+		assert.NoError(t, tracePublisher.ForceFlush(ctx))
+		assert.Len(t, exp.GetSpans(), 1)
 	})
 
 	t.Run("failure", func(t *testing.T) {
-		names, err := client.ListDatabaseNames(context.Background(), bson.M{})
+		exp.Reset()
+		names, err := client.ListDatabaseNames(ctx, bson.M{})
 		assert.Error(t, err)
 		assert.Empty(t, names)
+		assert.NoError(t, tracePublisher.ForceFlush(ctx))
+		assert.Len(t, exp.GetSpans(), 1)
 	})
 }

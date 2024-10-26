2700c5506eed773db3a9adbfa24348726843b149
Sotirios Mantziaris <smantziaris@gmail.com>
2024-10-26 08:49:06 +0300

    Logging refactoring

--- observability/integration_test.go
+++ observability/integration_test.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/beatlabs/patron/observability/log"
 	"github.com/stretchr/testify/require"
 )
 
@@ -13,7 +14,11 @@ func TestSetup(t *testing.T) {
 	t.Setenv("OTEL_EXPORTER_OTLP_INSECURE", "true")
 	ctx := context.Background()
 
-	got, err := Setup(ctx, Config{})
+	got, err := Setup(ctx, Config{
+		LogConfig: log.Config{
+			Level: "debug",
+		},
+	})
 	require.NoError(t, err)
 
 	require.NoError(t, got.Shutdown(ctx))
